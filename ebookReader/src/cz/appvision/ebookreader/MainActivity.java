87941786bb25eff893195f2305a3a716634863cb
Vojtech Toth <granko87@gmail.com>
2014-01-17 21:40:29 +0100

    ble

--- ebookReader/src/cz/appvision/ebookreader/MainActivity.java
+++ ebookReader/src/cz/appvision/ebookreader/MainActivity.java
@@ -3,18 +3,13 @@ package cz.appvision.ebookreader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.mcsoxford.rss.RSSFeed;
-import org.mcsoxford.rss.RSSItem;
-import org.mcsoxford.rss.RSSReader;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -22,7 +17,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 import cz.appvision.ebookreader.image.ImageCache;
 import cz.appvision.ebookreader.image.ImageCache.ImageCacheParams;
 import cz.appvision.ebookreader.image.ImageFetcher;
@@ -33,20 +27,21 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 	LinearLayout menuToolbar;
 	List<ImageView> buttons;
 	RelativeLayout root;
-	WebView webView;
 	ListView magazineListView;
 	private ImageFetcher imageFetcher;
 	private ProgressBar progressBar;
-	private RSSFeedArrayAdapter rssAdapter;
+	private View oNasLayout;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		ImageCacheParams cacheParams = new ImageCacheParams(this, ImageCache.DISK_CACHE_FILE);
+
+		ImageCacheParams cacheParams = new ImageCacheParams(this,
+				ImageCache.DISK_CACHE_FILE);
 		cacheParams.setMemCacheSizePercent(ImageCache.MEM_CACHE_20_PERCENT);
-		ImageCache.getInstance(getSupportFragmentManager(), cacheParams );
+		ImageCache.getInstance(getSupportFragmentManager(), cacheParams);
 		imageFetcher = new ImageFetcher(this);
 		imageFetcher.addImageCache(getSupportFragmentManager(), cacheParams);
 
@@ -69,33 +64,26 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 		menuOption3.setOnClickListener(this);
 		menuOption4.setOnClickListener(this);
 
-	}
+		LayoutInflater inflater = getLayoutInflater();
+		oNasLayout = inflater.inflate(R.layout.o_nas, null);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		onClick(menuOption1);
+
 	}
 
 	@Override
 	public void onClick(View v) {
+
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+
+		v.setSelected(true);
+		deselectButtons((ImageView) v);
 		switch (v.getId()) {
 		case R.id.toolbarMenuOption1:
 
-			if (webView == null){
-			webView = new WebView(this);
-
-			webView.getSettings().setBuiltInZoomControls(true);
-			webView.getSettings().setLoadWithOverviewMode(true);
-			webView.getSettings().setUseWideViewPort(true);
-
-			webView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-					ViewGroup.LayoutParams.MATCH_PARENT));
-
-			webView.loadUrl("http:\\www.sme.sk");
-			}
-
+			CustomApp app = (CustomApp) getApplication();
+			WebView webView = app.getWebView();
 			contentFrameLayout.removeAllViews();
 			contentFrameLayout.addView(webView);
 
@@ -103,70 +91,54 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 
 		case R.id.toolbarMenuOption2:
 			contentFrameLayout.removeAllViews();
+			BookShelfFragment bookShelfFragment = new BookShelfFragment();
+
+			transaction.replace(R.id.contentFrameLayout, bookShelfFragment);
+
+			transaction.commit();
 			break;
-			
+
 		case R.id.toolbarMenuOption3:
-			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			progressBar = new ProgressBar(this);
-			progressBar.setLayoutParams(params);
-			magazineListView = new ListView(this);
-			magazineListView.setLayoutParams(params);
-//			new RetrieveFeedTask().execute("http://feeds.bbci.co.uk/news/world/rss.xml");
-			new RetrieveFeedTask().execute("http://www.ctv.ca/feeds/mRssService.aspx?id=979");  
+
 			contentFrameLayout.removeAllViews();
-			contentFrameLayout.addView(progressBar);
+			// contentFrameLayout.addView(progressBar);
 			break;
-			
+
 		case R.id.toolbarMenuOption4:
 			contentFrameLayout.removeAllViews();
+			contentFrameLayout.addView(oNasLayout);
+
 			break;
 		}
 
 	}
-	
+
 	@Override
 	protected void onResume() {
 		imageFetcher.setExitTasksEarly(false);
 		super.onResume();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		imageFetcher.setExitTasksEarly(true);
 		imageFetcher.flushCache();
 		super.onPause();
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		imageFetcher.closeCache();
 		super.onDestroy();
 	}
-	
-	private class RetrieveFeedTask extends AsyncTask<String, Void, RSSFeed> {
 
-		@Override
-		protected RSSFeed doInBackground(String... urls) {
-			try {
-			String url= urls[0];
-			RSSReader reader = new RSSReader();
-			RSSFeed feed = reader.load(url);
-			reader.close();
-			return feed;
-			
-			} catch (Exception e) {
-	            return null;
-	        }
+	private void deselectButtons(ImageView iv) {
+		for (ImageView imageView : buttons) {
+			if (imageView != iv) {
+				imageView.setSelected(false);
+			}
 		}
-		
-		  protected void onPostExecute(RSSFeed feed) {
-			rssAdapter = new RSSFeedArrayAdapter(MainActivity.this, R.layout.rss_feed_item, feed, imageFetcher);
-			magazineListView.setAdapter(rssAdapter);
-			contentFrameLayout.removeAllViews();
-			contentFrameLayout.addView(magazineListView);
-		    }
 
 	}
-
 
 }
